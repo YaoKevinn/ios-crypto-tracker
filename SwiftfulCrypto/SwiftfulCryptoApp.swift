807371168c5ad44f8904bdac9036441f3cc9f6bd
YaoKevinn <vasty85817@gmail.com>
2021-12-14 01:43:30 -0300

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by YaoKevinn on 14/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
